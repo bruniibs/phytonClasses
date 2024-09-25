146699d308bc70e91ed8b0619cf0598868b8106e
boo <128152530+bruniibs@users.noreply.github.com>
2024-09-24 23:30:59 -0300

    Create exercicios.py

---
+++ exercicios.py
@@ -0,0 +1,6 @@
+# exercicio 3 algoritmos guanabara
+nome_fun = str(input('insira o nome do funcionario'))
+salario = float('1850,45')
+
+msg = f'O funcionario {nome_fun} tem um salario de {salario} em setembro'
+print(msg)
